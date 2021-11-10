39b887498291c723aa98b8415bd4222b6b78a8bb
hussain18 <talib.hussain18@vit.edu>
2021-11-10 20:07:32 +0530

    User authentication fixes

---
+++ server/api/index.js
@@ -0,0 +1,9 @@
+const authRouter = require('./auth');
+const frontEndRouter = require('./frontEnd');
+const chainRouter = require('./chain');
+
+module.exports = {
+  authRouter,
+  frontEndRouter,
+  chainRouter,
+};
