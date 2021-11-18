d70d5d711ed26a329b45973abb392e8d5b687d27
sunitapt <sunita.patil18@vit.edu>
2021-11-19 01:35:45 +0530

    Feat:Profile Info

--- frontEnd/src/pages/profile.js
+++ frontEnd/src/pages/profile.js
@@ -30,28 +30,27 @@ import {
 const tabs = [
     { name: 'Profile', href: '#', current: true },
     { name: 'Calendar', href: '#', current: false },
-    { name: 'Recognition', href: '#', current: false },
+    // { name: 'Recognition', href: '#', current: false },
   ]
 
-const profile = {
-    name: 'Ricardo Cooper',
+  const profile = {
+    name: 'Sunita Patil',
     imageUrl:
-      'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     coverImageUrl:
       'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
     about: `
-      <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
-      <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
+      <p>Hello there I am sunita patil. pursuing my education in Vishwakarma Institute of Technology Pune.I am a final year student and coding enthusiast.Hello there I am sunita patil. pursuing my education in Vishwakarma Institute of Technology Pune.I am a final year student and coding enthusiast.</p>
     `,
     fields: {
-      Phone: '(555) 123-4567',
-      Email: 'ricardocooper@example.com',
-      Title: 'Senior Front-End Developer',
+      Phone: '(+91) 12389-45697',
+      Email: 'sunita.patil18@vit.edu',
+      Title: 'Front-End Developer',
       Team: 'Product Development',
-      Location: 'San Francisco',
-      Sits: 'Oasis, 4th floor',
-      Salary: '$145,000',
-      Birthday: 'June 8, 1990',
+      Location: 'Bengaluru',
+      Sits: 'India',
+      Salary: '$111000',
+      Birthday: 'May 1, 2000',
     },
   }
   
